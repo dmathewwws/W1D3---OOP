f02b6c255bb43a774c95e0ae2d71da5942a08bd1
Daniel Mathews <dannyl.mathews@gmail.com>
2015-10-20 11:36:53 -0700

    v1

---
+++ W1D3 - OOP/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  W1D3 - OOP
+//
+//  Created by Daniel Mathews on 2015-10-20.
+//  Copyright © 2015 Daniel Mathews. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        
+    }
+    return 0;
+}
